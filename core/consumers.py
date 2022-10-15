2a33d3fc336168a29c62d9dd9ba98f5b739e43cd
awodi-emmanuel <emmanuelawodi25@gmail.com>
2022-10-15 17:25:24 +0100

    url added to ASGI app

--- core/consumers.py
+++ core/consumers.py
@@ -3,7 +3,7 @@ from channels.db import database_sync_to_async
 from asgiref.sync import async_to_sync, sync_to_async
 from channels.layers import get_channel_layer
 import json
-from models.implementation import Notifications
+from core.models.implementation import Notifications
 
 @database_sync_to_async
 def get_user(user_id):
@@ -23,12 +23,52 @@ def create_notification(receiver, typeof="task_created", status="unread"):
 
 class NotificationConsumer(AsyncWebsocketConsumer):
     async def websocket_connect(self, event):
-            print('connected', event)
-            print('Am i finally here')
-            print(self.scope['user'].id)
+            
+            print(self.scope)
             await self.accept()
             
             await self.send(json.dump({
                 "type":"websocket.send",
-                "text":"hello",
+                "text":"hello world",
             }))
+            self.room_name='test_consumer'
+            self.room_group_name='test_consumer_group'
+            await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+            
+            self.send({
+                "type": "websocket.send",
+                "text":"room made"
+            })
+            
+            
+    async def websocket_receive(self, event):
+            print(event)
+            data_to_get=json.loads(event['text'])
+            user_to_get=await get_user(int(data_to_get))
+            print(user_to_get)
+            get_of=await create_notification(user_to_get)
+            self.room_group_name='test_consumer_group'
+            channel_layer=get_channel_layer()
+            await (channel_layer.group_send) (
+                self.room_group_name,
+                {
+                    
+                    "type":"send_notification",
+                    'value':json.dumps(get_of)
+                }
+            )
+            
+            print('receive', event)
+            
+                
+    async def websocket_disconnect(self, event):
+            print('disconnect', event)
+                
+          
+    async def send_notification(self, event):
+            await self.send(json.dumps({
+                "type": "websocket.send",
+                "data":event
+            }))
+            print('I am here')
+            print(event)
